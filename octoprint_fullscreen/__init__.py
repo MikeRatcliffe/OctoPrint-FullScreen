c08633c434d124e5648c3e58a81f1ec91af445d8
Mike Ratcliffe <michael@ratcliffefamily.org>
2024-02-19 23:18:17 +0000

    build: Indent using spaces, not tabs

--- octoprint_fullscreen/__init__.py
+++ octoprint_fullscreen/__init__.py
@@ -2,48 +2,53 @@
 from __future__ import absolute_import
 import octoprint.plugin
 
-class FullscreenPlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.TemplatePlugin):
 
-	def get_settings_defaults(self):
-		return dict()
+class FullscreenPlugin(
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.TemplatePlugin,
+):
 
-	def get_assets(self):
-		return dict(
-			js=["js/fullscreen.js", "js/jquery-fullscreen.js"],
-			css=["css/fullscreen.css"],
-			less=[]
-		)
+    def get_settings_defaults(self):
+        return dict()
 
-	def get_template_configs(self):
-		files = [
-			dict(type="generic", template="fullscreen.jinja2", custom_bindings=True)
-		]
+    def get_assets(self):
+        return dict(
+            js=["js/fullscreen.js", "js/jquery-fullscreen.js"],
+            css=["css/fullscreen.css"],
+            less=[],
+        )
 
-		return files
+    def get_template_configs(self):
+        files = [
+            dict(type="generic", template="fullscreen.jinja2", custom_bindings=True)
+        ]
 
+        return files
 
-	def get_update_information(self):
-		return dict(
-			fullscreen=dict(
-				displayName="Fullscreen Plugin",
-				displayVersion=self._plugin_version,
-				type="github_release",
-				user="MikeRatcliffe",
-				repo="OctoPrint-FullScreen",
-				current=self._plugin_version,
-				pip="https://github.com/MikeRatcliffe/OctoPrint-FullScreen/archive/{target_version}.zip"
-			)
-		)
+    def get_update_information(self):
+        return dict(
+            fullscreen=dict(
+                displayName="Fullscreen Plugin",
+                displayVersion=self._plugin_version,
+                type="github_release",
+                user="MikeRatcliffe",
+                repo="OctoPrint-FullScreen",
+                current=self._plugin_version,
+                pip="https://github.com/MikeRatcliffe/OctoPrint-FullScreen/archive/{target_version}.zip",
+            )
+        )
+
 
 __plugin_name__ = "Fullscreen Plugin"
 __plugin_pythoncompat__ = ">=2.7,<4"
-def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = FullscreenPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = FullscreenPlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
--- octoprint_fullscreen/static/js/fullscreen.js
+++ octoprint_fullscreen/static/js/fullscreen.js
@@ -7,121 +7,121 @@
  */
 var onceOpenInlineFullscreen = false;
 if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
-	window.location.hash = window.location.hash.replace('-fullscreen-open', '').substr(1);
-	onceOpenInlineFullscreen = true;
+  window.location.hash = window.location.hash.replace('-fullscreen-open', '').substr(1);
+  onceOpenInlineFullscreen = true;
 }
 
 $(function() {
-	function FullscreenViewModel(parameters) {
-		var self = this;
-		var $body = $('body');
-		var $container, $fullscreenContainer;
+  function FullscreenViewModel(parameters) {
+    var self = this;
+    var $body = $('body');
+    var $container, $fullscreenContainer;
 
-		self.tempModel = parameters[0];
-		self.printer = parameters[1];
-		self.printer.fsp = {};
+    self.tempModel = parameters[0];
+    self.printer = parameters[1];
+    self.printer.fsp = {};
 
-		self.printer.fsp.printLayerProgress = ko.observable('');
-		self.printer.fsp.hasLayerProgress = ko.observable(false);
+    self.printer.fsp.printLayerProgress = ko.observable('');
+    self.printer.fsp.hasLayerProgress = ko.observable(false);
 
-		self.printer.fsp.isFullscreen = ko.observable(false);
-		self.printer.fsp.fullscreen = function() {
-			$fullscreenContainer.toggleFullScreen();
-		}
+    self.printer.fsp.isFullscreen = ko.observable(false);
+    self.printer.fsp.fullscreen = function() {
+      $fullscreenContainer.toggleFullScreen();
+    }
 
-		self.onStartupComplete = function() {
-			var $webcam = $('#webcam_image');
-			var $info = $('#fullscreen-bar');
-	
-			var containerPlaceholder = document.querySelector('#webcam,#webcam_container,#classicwebcam_container');
-			if (!containerPlaceholder) {
-				return;
-			}
+    self.onStartupComplete = function() {
+      var $webcam = $('#webcam_image');
+      var $info = $('#fullscreen-bar');
 
-			var containerPlaceholderSelector = `#${containerPlaceholder.id}`;
-			if ($('.webcam_fixed_ratio').length > 0) {
-				$container = $(containerPlaceholderSelector + ' .webcam_fixed_ratio');
-				$fullscreenContainer = $(containerPlaceholderSelector + ' #webcam_rotator');
-			} else {
-				$container = $(containerPlaceholderSelector + ' #webcam_rotator');
-				$fullscreenContainer = $(containerPlaceholderSelector + ' #classicwebcam_container');
-			}
+      var containerPlaceholder = document.querySelector('#webcam,#webcam_container,#classicwebcam_container');
+      if (!containerPlaceholder) {
+        return;
+      }
 
-			var touchtime = 0;
-			$webcam.on("click", function() {
-				if (touchtime === 0) {
-					touchtime = new Date().getTime();
-				} else {
-					if (((new Date().getTime()) - touchtime) < 800) {
-						$body.toggleClass('inlineFullscreen');
-						$container.toggleClass("inline fullscreen");
-	
-						if ($body.hasClass('inlineFullscreen')) {
-							history.pushState('', null, window.location.hash + '-fullscreen-open');
-						} else {
-							if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
-								history.pushState('', null, window.location.hash.replace('-fullscreen-open', ''));
-							}
-						}
-	
-						if(self.printer.fsp.isFullscreen()) {
-							$fullscreenContainer.toggleFullScreen();
-						}
-						touchtime = 0;
-					} else {
-						touchtime = new Date().getTime();
-					}
-				}
-			});
+      var containerPlaceholderSelector = `#${containerPlaceholder.id}`;
+      if ($('.webcam_fixed_ratio').length > 0) {
+        $container = $(containerPlaceholderSelector + ' .webcam_fixed_ratio');
+        $fullscreenContainer = $(containerPlaceholderSelector + ' #webcam_rotator');
+      } else {
+        $container = $(containerPlaceholderSelector + ' #webcam_rotator');
+        $fullscreenContainer = $(containerPlaceholderSelector + ' #classicwebcam_container');
+      }
 
-			if (onceOpenInlineFullscreen) {
-				setTimeout(function() {
-					touchtime = new Date().getTime();
-					$webcam.trigger("click");
-					onceOpenInlineFullscreen = false;
-				}, 100);
-			}
+      var touchtime = 0;
+      $webcam.on("click", function() {
+        if (touchtime === 0) {
+          touchtime = new Date().getTime();
+        } else {
+          if (((new Date().getTime()) - touchtime) < 800) {
+            $body.toggleClass('inlineFullscreen');
+            $container.toggleClass("inline fullscreen");
+  
+            if ($body.hasClass('inlineFullscreen')) {
+              history.pushState('', null, window.location.hash + '-fullscreen-open');
+            } else {
+              if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
+                history.pushState('', null, window.location.hash.replace('-fullscreen-open', ''));
+              }
+            }
+  
+            if(self.printer.fsp.isFullscreen()) {
+              $fullscreenContainer.toggleFullScreen();
+            }
+            touchtime = 0;
+          } else {
+            touchtime = new Date().getTime();
+          }
+        }
+      });
 
-			$info.insertAfter($container);
-			$(".print-control #job_pause").clone().appendTo("#fullscreen-bar .user-buttons").attr('id', 'job_pause_clone');
-	
-			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-print-info").get(0));
-			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-buttons").get(0));
-			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-progress-bar").get(0));
-		}
+      if (onceOpenInlineFullscreen) {
+        setTimeout(function() {
+          touchtime = new Date().getTime();
+          $webcam.trigger("click");
+          onceOpenInlineFullscreen = false;
+        }, 100);
+      }
 
-		self.onDataUpdaterPluginMessage = function (plugin, data) {
-			if (plugin.indexOf('DisplayLayerProgress') !== -1) {
-				if (!self.printer.fsp.hasLayerProgress()) {
-					self.printer.fsp.hasLayerProgress(true);
-				}
+      $info.insertAfter($container);
+      $(".print-control #job_pause").clone().appendTo("#fullscreen-bar .user-buttons").attr('id', 'job_pause_clone');
+  
+      ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-print-info").get(0));
+      ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-buttons").get(0));
+      ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-progress-bar").get(0));
+    }
 
-				if (data.currentLayer && data.totalLayer) {
-					self.printer.fsp.printLayerProgress(data.currentLayer + ' / ' + data.totalLayer);
-				}
-			}
-		};
+    self.onDataUpdaterPluginMessage = function (plugin, data) {
+      if (plugin.indexOf('DisplayLayerProgress') !== -1) {
+        if (!self.printer.fsp.hasLayerProgress()) {
+          self.printer.fsp.hasLayerProgress(true);
+        }
 
-		self.formatBarTemperatureFullscreen = function(toolName, actual, target) {
-			var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+        if (data.currentLayer && data.totalLayer) {
+          self.printer.fsp.printLayerProgress(data.currentLayer + ' / ' + data.totalLayer);
+        }
+      }
+    };
 
-			if (target) {
-				var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
-				output += sign + _.sprintf("%.1f&deg;C", target);
-			}
+    self.formatBarTemperatureFullscreen = function(toolName, actual, target) {
+      var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
 
-			return output;
-		};
+      if (target) {
+        var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
+        output += sign + _.sprintf("%.1f&deg;C", target);
+      }
 
-		$(document).bind("fullscreenchange", function() {
-			self.printer.fsp.isFullscreen($(document).fullScreen());
-		});
-	}
+      return output;
+    };
 
-	OCTOPRINT_VIEWMODELS.push({
-		construct: FullscreenViewModel,
-		dependencies: ["temperatureViewModel", "printerStateViewModel", "controlViewModel"],
-		optional: [],
-		elements: ["#fullscreen-tool-info"]
-	});
+    $(document).bind("fullscreenchange", function() {
+      self.printer.fsp.isFullscreen($(document).fullScreen());
+    });
+  }
+
+  OCTOPRINT_VIEWMODELS.push({
+    construct: FullscreenViewModel,
+    dependencies: ["temperatureViewModel", "printerStateViewModel", "controlViewModel"],
+    optional: [],
+    elements: ["#fullscreen-tool-info"]
+  });
 });
