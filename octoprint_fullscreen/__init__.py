1a24b7d650fc1ad56d575725f88f5debd60f828c
Mike Ratcliffe <michael@ratcliffefamily.org>
2024-02-24 22:18:24 +0000

    feat: Added settings: position, font-size, background-color, foreground-color and border color

--- octoprint_fullscreen/__init__.py
+++ octoprint_fullscreen/__init__.py
@@ -9,19 +9,64 @@ class FullscreenPlugin(
     octoprint.plugin.TemplatePlugin,
 ):
 
-    def get_settings_defaults(self):
-        return dict()
-
     def get_assets(self):
         return dict(
-            js=["js/fullscreen.js"],
-            css=["css/fullscreen.css"],
+            js=["js/colorpicker.js", "js/fullscreen.js"],
+            css=["css/colorpicker.css", "css/fullscreen.css"],
             less=[],
+        )
+
+    def get_settings_defaults(self):
+        return dict(
+            installed_version=self._plugin_version,
+            font_size="14px",
+            font_sizes=[
+                "10px",
+                "12px",
+                "14px",
+                "16px",
+                "18px",
+                "20px",
+                "22px",
+                "24px",
+                "26px",
+                "28px",
+                "30px",
+            ],
+            position="topleft",
+            positions=[
+                {"name": "Top Left", "value": "topleft"},
+                {"name": "Top Middle", "value": "topmiddle"},
+                {"name": "Top Right", "value": "topright"},
+                {"name": "Bottom Right", "value": "bottomright"},
+                {"name": "Bottom Middle", "value": "bottommiddle"},
+                {"name": "Bottom Left", "value": "bottomleft"},
+            ],
+            color_fg="rgba(255, 255, 255, 1)",
+            color_bg="rgba(0, 0, 0, 0.25)",
+            color_border="rgba(68, 68, 68, 1)",
+        )
+
+    def get_template_vars(self):
+        return dict(
+            installed_version=self._settings.get(["installed_version"]),
+            font_size=self._settings.get(["font_size"]),
+            font_sizes=self._settings.get(["font_sizes"]),
+            position=self._settings.get(["position"]),
+            positions=self._settings.get(["positions"]),
+            color_fg=self._settings.get(["color_fg"]),
+            color_bg=self._settings.get(["color_bg"]),
+            color_border=self._settings.get(["color_border"]),
         )
 
     def get_template_configs(self):
         files = [
-            dict(type="generic", template="fullscreen.jinja2", custom_bindings=True)
+            dict(
+                type="generic",
+                template="fullscreen.jinja2",
+                custom_bindings=True,
+            ),
+            dict(type="settings", custom_bindings=False),
         ]
 
         return files
--- octoprint_fullscreen/static/js/fullscreen.js
+++ octoprint_fullscreen/static/js/fullscreen.js
@@ -23,14 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
    * @param {Object} printer
    *        printerStateViewModel
    */
-  function FullscreenViewModel([tempModel, printer]) {
+  function FullscreenViewModel([tempModel, printerModel]) {
     let fullscreenContainer = null;
     let container = null;
     let touchtime = 0;
 
+    this.colorPickers = new Map();
+
     this.tempModel = tempModel;
-    this.printer = printer;
-    this.printer.fsp = {
+    this.printerModel = printerModel;
+    this.printerModel.fsp = {
       printLayerProgress: ko.observable(""),
       hasLayerProgress: ko.observable(false),
       isFullscreen: ko.observable(false),
@@ -39,10 +41,16 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     };
 
+    // We will populate settings later
+    this.settings = null;
+
     /**
      * Handle actions on Octoprint's startup completion.
      */
     this.onStartupComplete = () => {
+      this.settings =
+        this.printerModel.settings.settings.plugins.octoprint_fullscreen;
+
       const containerPlaceholder = document.querySelector(
         "#webcam,#webcam_container,#classicwebcam_container"
       );
@@ -52,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       const webcam = document.querySelector("#webcam_image");
-      const info = document.querySelector("#fullscreen-bar");
+      const bar = document.querySelector("#fullscreen-bar");
       const webcamFixedRatio = document.querySelector(".webcam_fixed_ratio");
 
       if (webcamFixedRatio) {
@@ -75,7 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 100);
       }
 
-      container.insertAdjacentElement("afterend", info);
+      container.insertAdjacentElement("afterend", bar);
 
       const buttonContainer = document.querySelector(
         "#fullscreen-bar .user-buttons"
@@ -86,22 +94,25 @@ document.addEventListener("DOMContentLoaded", () => {
       pauseButtonClone.id = "job_pause_clone";
       buttonContainer.appendChild(pauseButtonClone);
 
+      this.updateStyles();
+      this.initSwatches();
+
       ko.applyBindings(
-        this.printer,
+        this.printerModel,
         document.querySelector("#fullscreen-bar #fullscreen-print-info")
       );
       ko.applyBindings(
-        this.printer,
+        this.printerModel,
         document.querySelector("#fullscreen-bar #fullscreen-buttons")
       );
       ko.applyBindings(
-        this.printer,
+        this.printerModel,
         document.querySelector("#fullscreen-bar #fullscreen-progress-bar")
       );
     };
 
     /**
-     * Handles the plugin message for updating data.
+     * Handle messages from plugins.
      *
      * @param {string} plugin
      *        The identifier of the calling plugin
@@ -110,15 +121,33 @@ document.addEventListener("DOMContentLoaded", () => {
      */
     this.onDataUpdaterPluginMessage = (plugin, message) => {
       if (plugin.includes("DisplayLayerProgress")) {
-        if (!this.printer.fsp.hasLayerProgress()) {
-          this.printer.fsp.hasLayerProgress(true);
+        if (!this.printerModel.fsp.hasLayerProgress()) {
+          this.printerModel.fsp.hasLayerProgress(true);
         }
 
         if (message.currentLayer && message.totalLayer) {
           const progressMessage = `${message.currentLayer} / ${message.totalLayer}`;
-          this.printer.fsp.printLayerProgress(progressMessage);
+          this.printerModel.fsp.printLayerProgress(progressMessage);
         }
       }
+    };
+
+    /**
+     * Update settings when they are saved.
+     */
+    this.onSettingsBeforeSave = () => {
+      // Save color picker values to settings
+      for (const [name, colorPicker] of this.colorPickers) {
+        this.settings[name](colorPicker.color.rgbaString);
+      }
+    };
+
+    /**
+     * Tidy up settings
+     */
+    this.onSettingsHidden = () => {
+      this.updateStyles();
+      this.refreshColorPickers();
     };
 
     /**
@@ -167,12 +196,144 @@ document.addEventListener("DOMContentLoaded", () => {
           history.pushState("", null, hash.replace("-fullscreen-open", ""));
         }
 
-        if (this.printer.fsp.isFullscreen()) {
+        if (this.printerModel.fsp.isFullscreen()) {
           toggleFullScreen(fullscreenContainer);
         }
         touchtime = 0;
       } else {
         touchtime = now;
+      }
+    };
+
+    /**
+     * Updates styles. We can't use knockout to do this due to double bindings.
+     */
+    this.updateStyles = () => {
+      const bar = document.querySelector("#fullscreen-bar");
+      const fullscreenToggleBtn = document.querySelector(
+        "#fullscreen-bar #fullscreen-toggle"
+      );
+      const pauseBtn = document.querySelector("#job_pause_clone");
+
+      // Font size
+      bar.style.fontSize = this.settings.font_size();
+      fullscreenToggleBtn.style.fontSize = this.settings.font_size();
+      pauseBtn.style.fontSize = this.settings.font_size();
+
+      // Info box position
+      for (const position of this.settings.positions()) {
+        bar.classList.remove(position.value());
+      }
+      bar.classList.add(this.settings.position());
+
+      // Colors
+      bar.style.color = this.settings.color_fg();
+      bar.style.backgroundColor = this.settings.color_bg();
+      bar.style.borderWidth = "1px";
+      bar.style.borderStyle = "solid";
+      bar.style.borderColor = this.settings.color_border();
+
+      // TODO:
+      // Border colour
+      // Bar or box
+    };
+
+    /**
+     * Initialize color swatches
+     */
+    this.initSwatches = () => {
+      for (const { fieldName, swatchId, textId, saveColorFunc, alpha } of [
+        {
+          fieldName: "color_fg",
+          swatchId: "#octoprint-fullscreen-swatchfg",
+          textId: "#octoprint-fullscreen-swatchfgtext",
+          saveColorFunc: this.settings.color_fg,
+          alpha: false,
+        },
+        {
+          fieldName: "color_bg",
+          swatchId: "#octoprint-fullscreen-swatchbg",
+          textId: "#octoprint-fullscreen-swatchbgtext",
+          saveColorFunc: this.settings.color_bg,
+          alpha: true,
+        },
+        {
+          fieldName: "color_border",
+          swatchId: "#octoprint-fullscreen-swatchborder",
+          textId: "#octoprint-fullscreen-swatchbordertext",
+          saveColorFunc: this.settings.color_border,
+          alpha: true,
+        },
+      ]) {
+        const picker = document.querySelector(swatchId);
+        const pickerText = document.querySelector(textId);
+
+        // eslint-disable-next-line no-undef
+        const vanillaPicker = new octoprintFullscreenVanillaPicker({
+          fieldName,
+          parent: picker,
+          parentText: pickerText,
+          color: saveColorFunc(),
+          editorFormat: "rgb",
+          saveColorFunc,
+          cancelButton: true,
+          alpha,
+        });
+
+        /**
+         * Populate swatch and swatch text
+         */
+        vanillaPicker.updateSwatch = function () {
+          // Inside this handler `this` is in the context of the swatch
+          this.settings.parent.style.backgroundColor = this.color.rgbaString;
+          this.settings.parentText.value = `${this.color.hex} ${this.color.rgbaString}`;
+        };
+
+        // Store `this.refreshColorPickers()` because `closeHandler()` has no
+        // access to `this`.
+        const refreshColorPickers = this.refreshColorPickers;
+
+        // Add custom "onClose" functionality:
+        vanillaPicker._closeHandler = vanillaPicker.closeHandler;
+        vanillaPicker.closeHandler = function (e) {
+          // Inside this handler `this` is in the context of the swatch
+          if (
+            e.target.dataset.testId === "settings-save" ||
+            e.type === "focusin" ||
+            (e.type === "mousedown" && e.target !== this._domOkay)
+          ) {
+            return;
+          }
+
+          // Call original close handler
+          this._closeHandler(e);
+
+          if (e.target === this._domOkay) {
+            // Apply color to swatch
+            this.updateSwatch();
+          } else {
+            // Go back to original color
+            refreshColorPickers();
+          }
+        };
+
+        // Populate swatch and swatch text
+        vanillaPicker.updateSwatch();
+
+        // Store the swatch
+        this.colorPickers.set(fieldName, vanillaPicker);
+      }
+    };
+
+    /**
+     * If a settings dialog is cancelled we need to change all color swatches to
+     * match the colors in settings.
+     */
+    this.refreshColorPickers = () => {
+      // Restore color swatch and picker values from settings
+      for (const [name, colorPicker] of this.colorPickers) {
+        colorPicker.setColor(this.settings[name]());
+        colorPicker.updateSwatch();
       }
     };
 
@@ -212,17 +373,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     document.addEventListener("fullscreenchange", () => {
-      this.printer.fsp.isFullscreen(!!document.fullscreenElement);
+      this.printerModel.fsp.isFullscreen(!!document.fullscreenElement);
     });
   }
 
   OCTOPRINT_VIEWMODELS.push({
     construct: FullscreenViewModel,
-    dependencies: [
-      "temperatureViewModel",
-      "printerStateViewModel",
-      "controlViewModel",
-    ],
+    dependencies: ["temperatureViewModel", "printerStateViewModel"],
     optional: [],
     elements: ["#fullscreen-tool-info"],
   });
