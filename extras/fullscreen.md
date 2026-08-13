d216bed809cfb11a5dd9d952e80bf927a363655d
Mike Ratcliffe <michael@ratcliffefamily.org>
2026-08-13 17:00:00 +0100

    chore: tabs to spaces

--- extras/fullscreen.md
+++ extras/fullscreen.md
@@ -5,8 +5,8 @@ id: fullscreen
 title: OctoPrint-Fullscreen
 description: Open webcam in fullscreen mode
 authors:
-- Mike Ratcliffe
-- Paul de Vries
+  - Mike Ratcliffe
+  - Paul de Vries
 license: AGPLv3
 
 # TODO
@@ -22,21 +22,21 @@ follow_dependency_links: false
 
 # TODO
 tags:
-- a list
-- of tags
-- that apply
-- to your plugin
-- (take a look at the existing plugins for what makes sense here)
+  - a list
+  - of tags
+  - that apply
+  - to your plugin
+  - (take a look at the existing plugins for what makes sense here)
 
 # TODO
 screenshots:
-- url: url of a screenshot
-  alt: alt-text of a screenshot
-  caption: caption of a screenshot
-- url: url of another screenshot
-  alt: alt-text of another screenshot
-  caption: caption of another screenshot
-- ...
+  - url: url of a screenshot
+    alt: alt-text of a screenshot
+    caption: caption of a screenshot
+  - url: url of another screenshot
+    alt: alt-text of another screenshot
+    caption: caption of another screenshot
+  - ...
 
 # TODO
 featuredimage: url of a featured image for your plugin
@@ -44,14 +44,14 @@ featuredimage: url of a featured image for your plugin
 compatibility:
   # list of compatible versions, for example 1.2.0. If left empty no specific version requirement will be assumed
   octoprint:
-  - 1.2.0
+    - 1.2.0
 
   # list of compatible operating systems, valid values are linux, windows, macos, leaving empty defaults to all
   os:
-  - linux
-  - windows
-  - macos
+    - linux
+    - windows
+    - macos
 ---
 
 **TODO**: Longer description of your plugin, configuration examples etc. This part will be visible on the page at
-http://plugins.octoprint.org/plugin/fullscreen/
+<http://plugins.octoprint.org/plugin/fullscreen/>
--- octoprint_fullscreen/__init__.py
+++ octoprint_fullscreen/__init__.py
@@ -6,44 +6,44 @@ class FullscreenPlugin(octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin,
                        octoprint.plugin.TemplatePlugin):
 
-	def get_settings_defaults(self):
-		return dict()
+    def get_settings_defaults(self):
+        return dict()
 
-	def get_assets(self):
-		return dict(
-			js=["js/fullscreen.js", "js/jquery-fullscreen.js"],
-			css=["css/fullscreen.css"],
-			less=[]
-		)
+    def get_assets(self):
+        return dict(
+            js=["js/fullscreen.js", "js/jquery-fullscreen.js"],
+            css=["css/fullscreen.css"],
+            less=[]
+        )
 
-	def get_template_configs(self):
-		files = [
-			dict(type="generic", template="fullscreen.jinja2", custom_bindings=True)
-		]
+    def get_template_configs(self):
+        files = [
+            dict(type="generic", template="fullscreen.jinja2", custom_bindings=True)
+        ]
 
-		return files
+        return files
 
 
-	def get_update_information(self):
-		return dict(
-			fullscreen=dict(
-				displayName="Fullscreen Plugin",
-				displayVersion=self._plugin_version,
-				type="github_release",
-				user="MikeRatcliffe",
-				repo="OctoPrint-FullScreen",
-				current=self._plugin_version,
-				pip="https://github.com/MikeRatcliffe/OctoPrint-FullScreen/archive/{target_version}.zip"
-			)
-		)
+    def get_update_information(self):
+        return dict(
+            fullscreen=dict(
+                displayName="Fullscreen Plugin",
+                displayVersion=self._plugin_version,
+                type="github_release",
+                user="MikeRatcliffe",
+                repo="OctoPrint-FullScreen",
+                current=self._plugin_version,
+                pip="https://github.com/MikeRatcliffe/OctoPrint-FullScreen/archive/{target_version}.zip"
+            )
+        )
 
 __plugin_name__ = "Fullscreen Plugin"
 __plugin_pythoncompat__ = ">=2.7,<4"
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = FullscreenPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = FullscreenPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
